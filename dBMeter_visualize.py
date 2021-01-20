c2710dc11b613cb346c44131290d2ca0f7d07229
MarcinEL <marcinlewandowski.poczta@gmail.com>
2021-01-20 16:00:32 +0100

    Update dBMeter_visualize.py

--- dBMeter_visualize.py
+++ dBMeter_visualize.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# plik z danymi 
 with open("/content/drive/MyDrive/dbmeter.txt") as f:
   n_cols = len(f.readline().split("\t"))
 
+# moc sygnału w danym zakresie częstliwości dla każdej sekundy pomiaru
 freqData = np.loadtxt('/content/drive/MyDrive/dbmeter.txt', skiprows=1, usecols=np.arange(3,n_cols+1))
+# poziom głoności w całym paśmie dla każdej sekundy pomiaru
 loudnessData = np.loadtxt('/content/drive/MyDrive/dbmeter.txt', skiprows=1, usecols=2)
+# etykiety danych do wizualiacji
 labels = np.loadtxt('/content/drive/MyDrive/dbmeter.txt', dtype=str, max_rows=1, usecols=np.arange(2,n_cols))
 
+# uśrednienie mocy sygnału w poszczególnych pasmach za cały okres pomiaru
 meanFreqData = np.mean(freqData,axis=0)
 
 '''
